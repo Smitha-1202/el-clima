3b6f4ef233c791bb8e4cc7ec6c7ad99a7d0641da
Shamsmitha <yshamsmitha@gmail.com>
2026-07-06 19:25:05 +0530

    added API

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function search(city){
+  // make api call and update the UI
+  let apikey = "72e52d46dd4874415072of2t313ba90b";
+  let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+  axios.get(apiurl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  search(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
